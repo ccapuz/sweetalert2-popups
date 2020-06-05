16339749c1eebb4e7730323f59dd533bdc8225b5
unknown <cjmcapuz@gmail.com>
2020-06-04 22:02:36 -0400

    save

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pop-ups</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <button id='norm'>Normal Pop-up</button>
     <button id='long-content'>Long Content Pop-up</button>
-    <button id='aria-labels'>Aria Labels</button>
+    <button id='aria-labels'>Aria Labels Pop-up</button>
+    <button id='aria-labels'>Top Right Pop-up</button>
 
     <script src="jquery-3.5.1.min.js"></script>
     <script src="sweetalert2.all.min.js"></script>
@@ -36,11 +38,10 @@
         $('#aria-labels').on('click', function(){
             Swal.fire({
             title: '<strong>HTML <u>example</u></strong>',
-            icon: 'info',
+            icon: 'question',
             html:
-                'You can use <b>bold text</b>, ' +
-                '<a href="//sweetalert2.github.io">links</a> ' +
-                'and other HTML tags',
+                'You can use <b>bold text</b>, <em>italicized</em>, ' +
+                '<a href="https://github.com/ccapuz/sweetalert2-popups">linking GitHub Repository</a> ',
             showCloseButton: true,
             showCancelButton: true,
             focusConfirm: false,
@@ -53,7 +54,15 @@
             })
         })
 
-
+        $('#long-content').on('click', function(){
+            Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+            })
+        })
 
     </script>
 
